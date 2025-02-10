0b6e623ee66631ecb2040b295037a7715ccdf345
Nicholas Monteiro <nick.developerdev@gmail.com>
2025-02-09 21:03:53 -0300

    Handicap Calc
    Signed-off-by: Nicholas Monteiro <nick.developerdev@gmail.com>

---
+++ HanddicapCalculator/src/Display.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class Display {
+    JFrame frame;
+    JButton button;
+    JTextField textField;
+    public Display(){
+        frame = new JFrame();
+        button = new JButton();
+        textField = new JTextField();
+
+
+
+    }
+
+}
