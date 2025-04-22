5355d45331df198d94280728a2d2cd3250d24525
Nicholas Monteiro <nick.developerdev@gmail.com>
2025-04-22 16:56:36 -0300

    sisCaixa
    Signed-off-by: Nicholas Monteiro <nick.developerdev@gmail.com>

--- SistemaCaixa/src/Produto.java
+++ SistemaCaixa/src/Produto.java
@@ -4,7 +4,27 @@ public class Produto {
     double valor;
 
     public Produto(String nome, int id, double valor){
+        this.nome = nome;
+        this.id = id;
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {}
+
+    public int getId() {
+        return id;
 
     }
+    public void setId(int id) {}
+
+    public double getValor() {
+        return valor;
+    }
+    public void setValor(double valor) {}
+
 
 }
---
+++ SistemaCaixa/src/sisCaixa.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class sisCaixa {
+
+    List<Produto> produtosList;
+
+    public sisCaixa() {
+        produtosList = new ArrayList<>();
+    }
+
+    public void addProduto(Produto produto) {
+        produtosList.add(produto);
+    }
+
+    public void removeProduto(Produto produto) {
+        produtosList.remove(produto);
+        System.out.println(STR."\{produto.getNome()} removido com sucesso");
+    }
+
+    public void listarProdutos() {
+        int seq = 0;
+        for (Produto produto : produtosList) {
+            seq++;
+            System.out.println(STR."\{seq}.\{produto.getNome()}");
+        }
+    }
+}
