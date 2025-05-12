feaceaf9435e523375b0f66314d2ab3b2c219e0e
Nicholas Monteiro <nick.developerdev@gmail.com>
2025-05-12 19:07:35 -0300

    function
    Signed-off-by: Nicholas Monteiro <nick.developerdev@gmail.com>

---
+++ Functions/SimplesComRetorno/src/dobrarNumero.java
@@ -0,0 +1,10 @@
+public class dobrarNumero {
+    public static void main(String[] args) {
+        System.out.println(numero(2));
+
+    }
+
+    public static int numero(int num){
+        return num*2;
+    }
+}
