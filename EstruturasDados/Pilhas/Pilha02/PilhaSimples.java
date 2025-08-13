120ae5bbbbf82ffa3527f57b3053075827508dcc
NickDevD <nickdevnick.developerdev@gmail.com>
2025-08-13 08:28:48 -0300

    up
    Signed-off-by: NickDevD <nickdevnick.developerdev@gmail.com>

---
+++ EstruturasDados/Pilhas/Pilha02/PilhaSimples.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+public class PilhaSimples {
+
+    public static void simularPilha() {
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
+
+/*
+ public class PilhaSimples {
+
+    public static void main(String[] args) {
+    	simularPilha();
+    }
+
+    public static void simularPilha() {
+    	// sua implementação aqui
+    	// Empilhe: 5, 8, 4, 7 (nesta ordem)
+    	// Desempilhe duas vezes
+    	// Consulte o topo
+    	// Desempilhe mais duas vezes
+	}
+}
+ */
